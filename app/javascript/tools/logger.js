ec47ea696cfd07c1448a88c82a4cb6700eb1ad5f
Marian13 <mariankostyk13895@gmail.com>
2024-04-13 19:06:20 +0300

    feat(logger): set up js-logger

---
+++ app/javascript/tools/logger.js
@@ -0,0 +1,33 @@
+/**
+ * Lightweight, unobtrusive, configurable JavaScript logger.
+ * - https://github.com/jonnyreeves/js-logger
+ */
+import Logger from "js-logger";
+
+/**
+ * NOTE:
+ *   Sets js-Logger default logging level.
+ *   Prefixes log message with a timestamp.
+ * - https://github.com/jonnyreeves/js-logger?tab=readme-ov-file#usage
+ * - https://github.com/jonnyreeves/js-logger?tab=readme-ov-file#default-log-handler-function
+ * - https://github.com/jonnyreeves/js-logger?tab=readme-ov-file#usedefaults
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+ *
+ * NOTE: js-Logger does NOT support custom colors.
+ * - https://github.com/jonnyreeves/js-logger/blob/master/src/logger.js
+ */
+Logger.useDefaults({
+  defaultLevel: Logger.INFO,
+  formatter: messages => messages.unshift(`[${new Date().toISOString()}]`)
+});
+
+/**
+ * NOTE: Exposes js-Logger globally in order to have a way to change log levels from browser console.
+ * For example:
+ *
+ *   Logger.setLevel(Logger.OFF);
+ *
+ */
+window.Logger = Logger;
+
+export default Logger;
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,20 @@
-# Pin npm packages by running ./bin/importmap
-
+##
+# NOTE: Default configuration added by `rails new`.
+# NOTE: Pin additional npm packages by running `./bin/importmap`.
+#
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+##
+# NOTE: Local modules.
+# - https://github.com/rails/importmap-rails?tab=readme-ov-file#local-modules
+#
+pin_all_from "app/javascript/tools", under: "tools", to: "tools"
+
+##
+# NOTE: Additional npm packages added by `./bin/importmap pin package`.
+#
+pin "js-logger" # @1.6.1
