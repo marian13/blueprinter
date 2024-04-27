18f64e5eee940cf30c95b9d4cf8b23191a59e303
Marian13 <mariankostyk13895@gmail.com>
2024-04-27 23:21:46 +0300

    fix(super_diff): disable SuperDiff due to exception

--- spec/support/initializers.rb
+++ spec/support/initializers.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-require "initializers/super_diff"
+##
+# TODO: Uncomment when the efollowing exception is fixed:
+#
+#   NoMethodError:
+#     undefined method `keys' for nil
+#   # /usr/local/bundle/gems/super_diff-0.12.1/lib/super_diff/core/inspection_tree.rb:71:in `insert_hash_inspection_of'
+#
+# require_relative "initializers/super_diff"
