40174b1887ecd64a968cbd7fa3a9ba82695a8d9f
Marian13 <mariankostyk13895@gmail.com>
2024-07-11 02:23:38 +0300

    chore(Taskfile): pass autocorrect to annotate task

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,12 @@
 class App < ApplicationRecord
   has_one_attached :archive
 end
+
+# == Schema Information
+#
+# Table name: apps
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
